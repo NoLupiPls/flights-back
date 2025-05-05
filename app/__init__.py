516ca378d8f9abf019cd7514166fe706ae270579
Тимофей <kalyakta@yandex.ru>
2025-05-05 20:47:45 +0700

    Revert "Added pfp set feature, improved flight creation, fixed blueprinting"
    This reverts commit 9666b5e2a150d0be26c846eee1203feda4667337.

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,11 @@
 from flask import Flask
-import flask_login
-from app.data.users import User
 
 
 def create_app(config=None):
     """Create and configure the Flask application."""
     app = Flask(__name__)
 
-    from flask_login import LoginManager
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.get(user_id)
-
     from app.endpoints.ping import ping_bp
-    from app.endpoints.flight import flight_api
-
-    app.register_blueprint(flight_api)
     app.register_blueprint(ping_bp)
 
     return app
