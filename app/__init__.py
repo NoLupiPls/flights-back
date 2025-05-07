29b907d3d29d81fc0af782594bcc7bc726989fac
Timofey Kalyak <kalyakta@yandex.ru>
2025-05-07 16:56:13 +0700

    deleted index.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,7 @@ def create_app(config=None):
     from app.endpoints.register import register_bp
     from app.endpoints.flight import flight_api
     from app.endpoints.account_verification import verify_api
-    from app.endpoints.index import index
 
-    app.register_blueprint(index)
     app.register_blueprint(ping_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(register_bp)
